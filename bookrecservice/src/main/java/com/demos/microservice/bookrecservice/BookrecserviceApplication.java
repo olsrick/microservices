220d04ad4b887c8f09603e0c770fb0a136613351
Juan <olsrick@gmail.com>
2018-05-04 18:38:25 +0200

    DFD

---
+++ bookrecservice/src/main/java/com/demos/microservice/bookrecservice/BookrecserviceApplication.java
@@ -0,0 +1,12 @@
+package com.demos.microservice.bookrecservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrecserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrecserviceApplication.class, args);
+	}
+}
